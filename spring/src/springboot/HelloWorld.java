844d2635c92539c5aab78e850f7a60b9f840c297
sangeetha2628 <59245245+sangeetha2628@users.noreply.github.com>
2022-08-24 08:19:35 +0530

    Add files via upload

---
+++ spring/src/springboot/HelloWorld.java
@@ -0,0 +1,16 @@
+package springboot;
+
+public class HelloWorld {
+	   private String message;
+
+	   public void setMessage(String message){
+	      this.message  = message;
+	   }
+	   public void getMessage(){
+	      System.out.println("Your Message : " + message);
+	   }
+	public void getMessage() {
+		// TODO Auto-generated method stub
+		
+	}
+	}
---
+++ spring/src/springboot/Main.java
@@ -0,0 +1,12 @@
+package springboot;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+   public static void main(String[] args) {
+      ApplicationContext context = new ClassPathXmlApplicationContext("Bean.xml");
+      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+      obj.getMessage();
+   }
+}
